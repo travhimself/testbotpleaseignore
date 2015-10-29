0f314a6799b51ff50dd16b6e20077e70c6bcb7e8
Travis Sisti <travhimself@gmail.com>
2015-10-29 13:03:30 -0600

    ohgod fixing indents

--- rekt_detector.py
+++ rekt_detector.py
@@ -4,11 +4,11 @@ import random
 @sopel.module.rule('Chanserv.*kicked.*')
 
 def rekt(bot, trigger):
-        rektlist = ['rektangle', 'tyranosaurus rekt', 'the good, the rekt, and the ugly', 'south by southrekt', 'brektfast']
-            rektselection = random.choice(rektlist)
+    rektlist = ['rektangle', 'tyranosaurus rekt', 'the good, the rekt, and the ugly', 'south by southrekt', 'brektfast']
+    rektselection = random.choice(rektlist)
 
-                notrektstring = u'\u2610 not rekt'
-                    rektstring = u'\u2611' + rektselection
+    notrektstring = u'\u2610 not rekt'
+    rektstring = u'\u2611' + rektselection
 
-                        bot.say(notrektstring)
-                            bot.say(rektstring)
+    bot.say(notrektstring)
+    bot.say(rektstring)
