e1c04306fb56edaf2e787006a4e6d73ce925389f
Travis Sisti <travhimself@gmail.com>
2015-10-29 13:00:19 -0600

    skeleton for rekt alert module

---
+++ rekt_detector.py
@@ -0,0 +1,14 @@
+import sopel.module
+import random
+
+@sopel.module.rule('Chanserv.*kicked.*')
+
+def rekt(bot, trigger):
+        rektlist = ['rektangle', 'tyranosaurus rekt', 'the good, the rekt, and the ugly', 'south by southrekt', 'brektfast']
+            rektselection = random.choice(rektlist)
+
+                notrektstring = u'\u2610 not rekt'
+                    rektstring = u'\u2611' + rektselection
+
+                        bot.say(notrektstring)
+                            bot.say(rektstring)
