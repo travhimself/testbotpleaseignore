8479478b5356912124d2ee01a9fe49a3395af03a
Michael Toymil <mike.toymil@gmail.com>
2015-10-30 13:59:30 -0600

    Welpcall debug run

---
+++ welp.py
@@ -0,0 +1,13 @@
+import sopel.module
+import sopel.tools.time
+import datetime
+
+
+@sopel.module.rule('.*welp.*')
+def record_welp(bot, trigger):
+    nick = trigger.nick
+    welp_count = bot.db.get_nick_value(nick, 'welp_count')
+    welp_count = welp_count + 1
+    bot.db.set_nick_value(nick, 'welp_count', welp_count)
+    bot.say(nick + ' has welped ' + str(welp_count) + 'times.')
+    bot.say('TIME DEBUG: ' + sopel.tools.time.format_time(time=datetime.datetime.now()))
