311978f2f11fa5f4552cea49558d5bb2eebbf398
Michael Toymil <mike.toymil@gmail.com>
2015-10-30 14:05:39 -0600

    zero fallback for welpcount

--- welp.py
+++ welp.py
@@ -6,7 +6,7 @@ import datetime
 @sopel.module.rule('.*welp.*')
 def record_welp(bot, trigger):
     nick = trigger.nick
-    welp_count = bot.db.get_nick_value(nick, 'welp_count')
+    welp_count = bot.db.get_nick_value(nick, 'welp_count') or 0
     welp_count = welp_count + 1
     bot.db.set_nick_value(nick, 'welp_count', welp_count)
     bot.say(nick + ' has welped ' + str(welp_count) + 'times.')
