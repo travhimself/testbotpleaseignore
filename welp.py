ad672e624cd150e0c790664335a63d681dfe5d1a
Michael Toymil <mike.toymil@gmail.com>
2015-10-30 15:54:47 -0600

    trav will shut this down one day because of me

--- welp.py
+++ welp.py
@@ -8,7 +8,7 @@ def welp_interval(bot):
     if bot.memory.contains('next_welpcall'):
         now = datetime.datetime.now()
         delta = now - bot.memory['next_welpcall']
-        if abs(delta.total_seconds) < 6:
+        if abs(delta.total_seconds()) < 6:
             if "#testchannelpleaseignore" in bot.channels:
                 # It's go time!
                 _set_next_welpcall(bot)
