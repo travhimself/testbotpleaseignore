44349fd4a6038285a2672b283db9ea4cee914bbf
Michael Toymil <mike.toymil@gmail.com>
2015-11-03 21:46:45 -0700

    hyphenator

--- portmanteau.py
+++ portmanteau.py
@@ -1,28 +1,19 @@
 import sopel.module
 import random
+from hyphenator import hyphenate_word
 
-
+# Uses http://nedbatchelder.com/code/modules/hyphenate.py
+# to break words up into parts and then shuffles and joins the pieces
+# into one word. Could be modified to output more than one word.
 @sopel.module.commands('portmanteau')
 def portmanteau(bot, trigger):
     if not trigger.group(2):
         bot.say('Need some words, doggles.')
         return
-    vowel_set = set(['a','e','i','o','u', 'y'])
     words = trigger.group(2).split(' ')
     pieces = []
     for word in words:
-        vowels = []
-        for i in xrange(0, len(word)):
-            c = word[i]
-            if c in vowel_set:
-                vowels.append(i)
-        
-        if len(vowels):
-            split_index = random.choice(vowels)
-        else:
-            split_index = len(word)/2
-
-        splitwords = [word[:split_index], word[split_index:],word] 
+        splitwords = hyphenate_word(word)
         pieces.append(random.choice(splitwords))
 
     random.shuffle(pieces)
